ff70bcbe9632adb11ed9a080d661e3c1a4e7c02f
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2019-10-15 23:50:04 -0400

    Starter files for practical08

---
+++ src/fibArm_starter_i.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""myMorseGenerator_solution.py: Program to use generator functions to put text into morse code."""
+
+__author__      = "ADD YOUR NAME!!!"
+__date__        = "16 Oct 2019"
+
+def printLine():
+    print("\t","-" * 50)
+#end of printLine()
+
+def fibsList(n):
+    """The Fibonacci Sequence to be used for comparison to your own sequence"""
+    # stavely's chapter 7
+    result_list = [ ]
+    a=1
+    b=1
+
+    for i in range(n):
+        result_list.append(a)
+        a, b = b, a + b
+    return result_list
+# end of FibList()
+
+
+
+def fibByArm(m, gRatio_flt):
+# The function to compute the fibonacci sequence based on the golden ratio
+# m is a previous term, and gRatio_flt is the Golden Ratio (Phi)
+    printLine()
+#   TODO: calculate your fibonacci sequence using the arm mesaurements (phi)
+# note: this function is called to calculate a single value of the sequence.
+# It returns this value to the function that called it.
+
+#end of fibByArm()
+
+
+
+
+
+def main():
+    print(" Welcome to the Fibonacci Sequence calculator using the Golden")
+    print(" Ratio which has been prepared from measurments of your arm. ")
+    printLine()
+    iterations_int = 15
+    print(" The Real Fibonacci Sequence for",iterations_int,"iterations:")
+    print(fibsList(iterations_int))
+# get user input (As floats) for both measurements
+#    sf_flt = float(input(" Length of shoulder tip of longest finger : "))
+#    ef_flt = float(input(" Length of elbow to tip of longest finger : "))
+
+#   TODO: add code to work with these measurments and call the fibByArm() function
+
+#end of main()
+
+
+
+# this is used for the math.ciel() function that rounds up values to nearest integer
+import math
+if __name__ == "__main__":
+	main()
---
+++ writing/statement.md
@@ -0,0 +1,3 @@
+#### Date: 15 October 2019
+#### Speaker statment (see DoGoodRobotics.pdf) for details about the talk.
+
